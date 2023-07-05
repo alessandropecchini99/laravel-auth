84b7affecbb47023053f74629947013998f94a97
alessandropecchini99 <pecchini99@gmail.com>
2023-07-05 20:06:42 +0200

    push

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+// bootstrap di lavarel, non bootstrapt di stile
+import "./bootstrap";
+
+// importo il mio file scss
+import "~resources/scss/app.scss";
+
+// traduce le img
+import.meta.glob(["../img/**"]);
+
+/* Import Bootstrap 5 */
+import * as bootstrap from "bootstrap";
